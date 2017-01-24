bd907afe6372f1b7a39930160cfa2a29e5806709
Ian White <ibwhite@gmail.com>
2017-01-24 16:55:39 -0500

    adding json helper for tests

--- stardao-core/src/main/java/io/stardog/stardao/jackson/JsonHelper.java
+++ stardao-core/src/main/java/io/stardog/stardao/jackson/JsonHelper.java
@@ -6,12 +6,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.stardog.stardao.core.Update;
 
+/**
+ * This helper lets you quickly construct objects for tests, including using single-quotes/unquoted field names
+ * to make it easier to construct JSON strings inline Java.
+ *
+ * It's not intended to be used for production purposes. Just testing.
+ */
 public class JsonHelper {
-    private static ObjectMapper MAPPER = new ObjectMapper()
+    public static ObjectMapper MAPPER = new ObjectMapper()
             .configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true)
             .configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true)
             .registerModule(new JavaTimeModule());
 
+    /**
+     * Create an object, given a JSON string and a class.
+     * @param json  JSON string
+     * @param klazz class of object to create
+     * @param <T>   type of object to create
+     * @return  object
+     */
     public static <T> T object(String json, Class<T> klazz) {
         try {
             return MAPPER.readValue(json, klazz);
@@ -20,10 +33,30 @@ public class JsonHelper {
         }
     }
 
+    /**
+     * Create an Update object, given the class of the update type
+     * @param json  JSON string
+     * @param klazz class of the update type
+     * @param <T>
+     * @return
+     */
     public static <T> Update<T> update(String json, Class<T> klazz) {
         try {
             JavaType type = MAPPER.getTypeFactory().constructParametricType(Update.class, klazz);
             return MAPPER.readValue(json, type);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Turn an object into JSON.
+     * @param object    object
+     * @return  JSON string of object
+     */
+    public static String json(Object object) {
+        try {
+            return MAPPER.writeValueAsString(object);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- stardao-core/src/test/java/io/stardog/stardao/jackson/JsonHelperTest.java
+++ stardao-core/src/test/java/io/stardog/stardao/jackson/JsonHelperTest.java
@@ -4,6 +4,8 @@ import io.stardog.stardao.core.TestModel;
 import io.stardog.stardao.core.Update;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.Assert.*;
 
 public class JsonHelperTest {
@@ -19,5 +21,11 @@ public class JsonHelperTest {
         Update<TestModel> update = JsonHelper.update("{'name':'Ian','active':true,email:null}", TestModel.class);
         assertTrue(update.getSetObject().getActive());
         assertTrue(update.getRemoveFields().contains("email"));
+    }
+
+    @Test
+    public void testJson() throws Exception {
+        TestModel model = TestModel.builder().name("Marty").birthday(LocalDate.of(1985, 10, 26)).build();
+        assertEquals("{\"name\":\"Marty\",\"birthday\":\"1985-10-26\"}", JsonHelper.json(model));
     }
 }
